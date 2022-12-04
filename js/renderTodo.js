5bb09bd5a8edd17d08985e9760396fbb78f58ae6
quokka-eating-carrots <ovzip7@gmail.com>
2022-12-04 16:47:36 +0900

    Fixed: 새로 추가하면 제일 아래로 가는 거 수정

--- js/renderTodo.js
+++ js/renderTodo.js
@@ -30,22 +30,24 @@ export const addBtnHandler = addBtn.addEventListener('click', async (e) => {
   let title = inputEl.value
   let order = (todoListEl.querySelectorAll('.todo').length) + 1
   const todo = await createTodo(title, order)
+  const newTodo = document.createElement('div')
+  newTodo.id = `${todo.id}`
+  newTodo.classList.add('todo')
+  newTodo.innerHTML = /* html */ `
+    <img src="./images/${bunnys()}" alt="토끼 프로필" />
+    <input value="${inputEl.value}" />
+    <div class="state-box">
+      <div class="state-btn">
+        <button type='text' class='material-symbols-outlined done'>done</button>
+        <button type='text' class='material-symbols-outlined delete'>delete</button>
+      </div>
+      <div class="editDate">${todo.updatedAt.slice(0, 10)}</div>
+    </div>
+  `
   if (title.length === 0) {
     alert("내용을 입력해 주세요")
   } else {
-    todoListEl.innerHTML += /* html */ `
-    <div class="todo" id="${todo.id}">
-      <img src="./images/${bunnys()}" alt="토끼 프로필" />
-      <input value="${inputEl.value}" />
-      <div class="state-box">
-        <div class="state-btn">
-          <button type='text' class='material-symbols-outlined done'>done</button>
-          <button type='text' class='material-symbols-outlined delete'>delete</button>
-        </div>
-        <div class="editDate">${todo.updatedAt.slice(0, 10)}</div>
-      </div>
-    </div>
-  `
+    todoListEl.prepend(newTodo)
   }
   inputEl.value = ''
 })
